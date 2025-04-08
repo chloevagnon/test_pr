1e47a493a521d37ce239701878bd2a757b39bd90
Camille Coux <camillecoux@users.noreply.github.com>
2025-04-08 11:50:50 +0200

    init repo

---
+++ dummy_script.R
@@ -0,0 +1,18 @@
+# here is some code to edit
+
+library(GGally)
+library(magrittr)
+
+data(airquality)
+
+pairs(airquality, panel = panel.smooth, main = "airquality data")
+GGally::ggpairs(airquality, ggplot2::aes(color = Month %>% as.factor()))
+GGally::ggpairs(iris, ggplot2::aes(color=Species)) 
+
+
+
+## Quick example, with and without colour
+data(flea)
+ggpairs(flea, columns = 2:4)
+ggpairs(flea, columns = 2:4, ggplot2::aes(colour = species))
+
